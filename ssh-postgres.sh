53c72fa4356bfeae8e5384a1e311706c37f34aba
pincoin <pincoin@users.noreply.github.com>
2019-01-20 23:45:47 +0900

    Update ssh-postgres.sh

--- ssh-postgres.sh
+++ ssh-postgres.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -x
 
+ufw='/usr/sbin/ufw'
+
 PORTS=("22" "5432")
 
 if [ $# -eq 2 ]; then
     if [ "$1" = "allow" ]; then
         for port in ${PORTS[@]}; do
-            ufw allow from $2 to any port $port proto tcp
+            $ufw allow from $2 to any port $port proto tcp
         done
     elif [ "$1" = "disallow" ]; then
         for port in ${PORTS[@]}; do
-            ufw delete allow from $2 to any port $port proto tcp
+            $ufw delete allow from $2 to any port $port proto tcp
         done
     fi
 fi
